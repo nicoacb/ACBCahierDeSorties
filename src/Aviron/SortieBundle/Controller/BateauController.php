7b80b02d86dfef420ed484841b659b3cdf00d92d
nicoacb <lenicolas.jolly@gmail.com>
2018-08-08 22:18:49 +0200

    Administration : remettre un bateau en service

--- src/Aviron/SortieBundle/Controller/BateauController.php
+++ src/Aviron/SortieBundle/Controller/BateauController.php
@@ -159,4 +159,34 @@ class BateauController extends Controller
         // On redirige vers la liste des bateaux
         return $this->redirectToRoute('aviron_bateaux_home');
     }
+
+    /**
+    * @Security("has_role('ROLE_ADMIN')")
+    */
+    public function remettreenserviceAction(Request $request, $id)
+    {
+        // On récupère l'entité du bateau correspondant à l'id $id
+        $bateau = $this->getDoctrine()
+            ->getManager()
+            ->getRepository('AvironSortieBundle:Bateau')
+            ->find($id);
+
+        // Si $bateau est null, l'id n'existe pas
+        if(null == $bateau) {
+            throw new NotFoundHttpException("Le bateau d'id ".$id." n'existe pas.");
+        }
+
+        $bateau->setDatehorsservice(null);
+
+        // On enregistre l'objet $sortie en base de données
+        $em = $this->GetDoctrine()->getManager();
+        $em->persist($bateau);
+        $em->flush();
+
+        // On affiche un message de validation
+        $request->getSession()->getFlashBag()->add('success', 'Bateau mis hors-service.');
+
+        // On redirige vers la liste des bateaux
+        return $this->redirectToRoute('aviron_bateaux_home');
+    }
 }
